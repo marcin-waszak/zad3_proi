da99f4fb65381924679c74cf0890c4e0448d825d
Marcin Waszak <M.Waszak@stud.elka.pw.edu.pl>
2015-05-18 18:22:25 +0200

    Without visitor

---
+++ zad3_proi/Schedule.cpp
@@ -0,0 +1,82 @@
+#include "Schedule.h"
+
+Schedule::~Schedule()
+{
+	size_t vectorSize = m_vector.size();
+	
+	for(size_t i = 0; i < vectorSize; i++)
+		erase(vectorSize - i - 1);
+}
+
+void Schedule::addForward(int distance)
+{
+	Direction* p_direction;
+	p_direction = new ForwardDirection(distance);
+
+	m_vector.push_back(p_direction);
+}
+
+void Schedule::addExit(int choice)
+{
+	Direction* p_direction;
+	p_direction = new ExitDirection(choice);
+
+	m_vector.push_back(p_direction);
+}
+
+void Schedule::addLeft()
+{
+	Direction* p_direction;
+	p_direction = new LeftDirection();
+
+	m_vector.push_back(p_direction);
+}
+
+void Schedule::addRight()
+{
+	Direction* p_direction;
+	p_direction = new RightDirection();
+
+	m_vector.push_back(p_direction);
+}
+
+void Schedule::insertForward(size_t position, int distance)
+{
+	Direction* p_direction;
+	p_direction = new ForwardDirection(distance);
+
+	m_vector.insert(m_vector.begin() + position, p_direction);
+}
+
+void Schedule::insertExit(size_t position, int choice)
+{
+	Direction* p_direction;
+	p_direction = new ExitDirection(choice);
+
+	m_vector.insert(m_vector.begin() + position, p_direction);
+}
+
+void Schedule::insertLeft(size_t position)
+{
+	Direction* p_direction;
+	p_direction = new LeftDirection();
+
+	m_vector.insert(m_vector.begin() + position, p_direction);
+}
+
+void Schedule::insertRight(size_t position)
+{
+	Direction* p_direction;
+	p_direction = new RightDirection();
+
+	m_vector.insert(m_vector.begin() + position, p_direction);
+}
+
+void Schedule::erase(size_t position)
+{
+	Direction* p_direction;
+	p_direction = m_vector[position];
+
+	m_vector.erase(m_vector.begin() + position);
+	delete p_direction;
+}
